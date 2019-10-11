d4dd1c156b1c3134fd621567da6b8a23411e8ca9
Bassem <Bsaleh103@gmail.com>
2019-10-11 22:35:34 +0200

    I added code for Trip Object and I prepared the data for SQLite using Room Library

--- app/src/main/java/com/group4/togolist/db/TripDao.java
+++ app/src/main/java/com/group4/togolist/db/TripDao.java
@@ -13,18 +13,25 @@ import java.util.List;
 
 @Dao
 public interface TripDao {
+
+    // method for entering new row int Trip Table
     @Insert
     void addTrip(Trip newTrip);
 
+    // get List for all Trips in SQLite Trip Table
     @Query("select * from Trip")
     LiveData<List<Trip>> getTrips();
 
+    // get List of Upcoming Trips
     @Query("select * from Trip where status = 'upcoming'")
     LiveData<List<Trip>> getUpcomingTrips();
+    // get List of Ended Trips
     @Query("select * from Trip where status = 'Ended'")
     LiveData<List<Trip>> getEndedTrips();
+    // get List of Delayed Trips
     @Query("select * from Trip where status = 'Delayed'")
     LiveData<List<Trip>> getDelayedTrips();
+    // get List of Canceled Trips
     @Query("select * from Trip where status = 'Canceled'")
     LiveData<List<Trip>> getCanceledTrips();
 
