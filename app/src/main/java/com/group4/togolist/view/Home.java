d95e928e54e77a2c00d221fa6fead4622a8f176b
alialfayed1 <alialfayed1@gmail.com>
2019-10-12 16:37:28 +0200

    add Splash Activity and run plane animation create home Activity and add java code to open 2 Activity Sign in , sign up

--- app/src/main/java/com/group4/togolist/view/Home.java
+++ app/src/main/java/com/group4/togolist/view/Home.java
@@ -1,16 +1,40 @@
 package com.group4.togolist.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.group4.togolist.R;
 
-public class Home extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    Button btn_SignIN, btn_createaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initcomponent();
+    }
+
+    private void initcomponent() {
+        btn_SignIN = findViewById(R.id.btn_SignIN);
+        btn_createaccount = findViewById(R.id.btn_createaccount);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_SignIN:
+                Intent SignIN = new Intent(Home.this, com.group4.togolist.view.SignIN.class);
+                startActivity(SignIN);
+                break;
+            case R.id.btn_createaccount:
+                Intent SignUP = new Intent(Home.this, com.group4.togolist.view.SignUP.class);
+                startActivity(SignUP);
+                break;
+        }
     }
 }
