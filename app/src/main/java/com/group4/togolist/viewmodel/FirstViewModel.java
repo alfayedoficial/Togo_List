d2de6f6da9ee6d47ccba195a87c43fb93fc90a72
alialfayed1 <alialfayed1@gmail.com>
2019-10-12 17:44:07 +0200

    add Splash Activity and run plane animation create home Activity and add java code to open 2 Activity Sign in , sign up

--- app/src/main/java/com/group4/togolist/viewmodel/FirstViewModel.java
+++ app/src/main/java/com/group4/togolist/viewmodel/FirstViewModel.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.Toast;
 
+import com.group4.togolist.view.SignIN;
+import com.group4.togolist.view.SignUP;
+
 import androidx.lifecycle.ViewModel;
 
 public class FirstViewModel extends ViewModel {
@@ -23,7 +26,7 @@ public class FirstViewModel extends ViewModel {
      * this method called when Sign in Button in the starting activity, it takes you to Sign In Activity
      */
     public void signIn(){
-        Intent signInIntent = new Intent(firstActivity,LoginActivity.class);
+        Intent signInIntent = new Intent(firstActivity, SignIN.class);
         firstActivity.startActivity(signInIntent);
     }
 
@@ -31,7 +34,7 @@ public class FirstViewModel extends ViewModel {
      * this method called when create an Account Button in the starting activity, it takes you to Register Activity
      */
     public void createAccount(){
-        Intent signUpIntent = new Intent (firstActivity,RegisterActivity.class);
+        Intent signUpIntent = new Intent (firstActivity, SignUP.class);
         firstActivity.startActivity(signUpIntent);
     }
 
