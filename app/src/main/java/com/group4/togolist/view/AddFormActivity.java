7ee996e0e3d2c581f01e05ebd3accc742f95bb27
alialfayed1 <alialfayed1@gmail.com>
2019-10-13 11:52:55 +0200

    Add Sign Up activity , Forget password , add comments to java classes , edit sign in  xml Activity

---
+++ app/src/main/java/com/group4/togolist/view/AddFormActivity.java
@@ -0,0 +1,16 @@
+package com.group4.togolist.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group4.togolist.R;
+
+public class AddFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_form);
+    }
+}
