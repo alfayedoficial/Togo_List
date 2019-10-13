e94b12dfde938a355a574eb6a923c162387ed2f7
alialfayed1 <alialfayed1@gmail.com>
2019-10-13 17:18:56 +0200

    Add Add auto complete ,

--- app/src/main/java/com/group4/togolist/view/AddFormActivity.java
+++ app/src/main/java/com/group4/togolist/view/AddFormActivity.java
@@ -1,17 +1,68 @@
 package com.group4.togolist.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 import com.group4.togolist.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddFormActivity extends AppCompatActivity {
+    LatLng latLang1;
+    LatLng latLang2;
+    Double lat1;
+    Double lat2;
+    Double long2;
+    Double long1;
+    String placeDestination;
+    String placeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_form);
+
+        final PlaceAutocompleteFragment autocompleteFragment1 = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.editText_startPoint);
+        autocompleteFragment1.getView().setBackgroundColor(Color.WHITE);
+        autocompleteFragment1.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                latLang1 = place.getLatLng();
+                placeName = (String) place.getName();
+                long1 = latLang1.longitude;
+                lat1 = latLang1.latitude;
+            }
+
+            @Override
+            public void onError(Status status) {
+
+                final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
+                        getFragmentManager().findFragmentById(R.id.editText_endPoint);
+                autocompleteFragment2.getView().setBackgroundColor(Color.WHITE);
+                autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+                        // TODO: Get info about the selected place.
+                        latLang2 = place.getLatLng();
+                        placeDestination = (String) place.getName();
+                        long2 = latLang2.longitude;
+                        lat2 = latLang2.latitude;
+
+                    }
+
+                    @Override
+                    public void onError(Status status) {
+
+                    }
+                });
+            }
+        });
     }
 
 }
--- app/src/main/java/com/group4/togolist/view/ForgetPasswordActivity.java
+++ app/src/main/java/com/group4/togolist/view/ForgetPasswordActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
