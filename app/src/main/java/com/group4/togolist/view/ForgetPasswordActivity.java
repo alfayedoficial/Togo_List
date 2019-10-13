ce9e4be78c66cef0a0793d3fe95b15b6205f6652
Laptop Shop <fatma_kamal@mans.edu.eg>
2019-10-13 15:55:50 +0200

    adding ForgetPassViewModel

--- app/src/main/java/com/group4/togolist/view/ForgetPasswordActivity.java
+++ app/src/main/java/com/group4/togolist/view/ForgetPasswordActivity.java
@@ -1,20 +1,85 @@
 package com.group4.togolist.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.group4.togolist.R;
+import com.group4.togolist.viewmodel.ForgetPassViewModel;
+import com.group4.togolist.viewmodel.RegisterViewModel;
 
-public class ForgetPasswordActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends AppCompatActivity implements View.OnClickListener {
     /**
      * Class do :
      * Created by Group 4 ITI (Eng/Bassen - Eng Fatma - Eng Ali)
      */
 
+    private ForgetPassViewModel forgetPassViewModel;
+    private Button btnResetPassword , btnCancel;
+    private TextView eTxtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        forgetPassViewModel = ViewModelProviders.of(this , new MyViewModelFactory(ForgetPasswordActivity.this)).get(ForgetPassViewModel.class);
+
+        initComponent();
+    }
+
+    private void initComponent() {
+
+        btnResetPassword = findViewById(R.id.btn_forget_password);
+        btnCancel = findViewById(R.id.btn_Cancel_forgetPass);
+
+        eTxtPassword = findViewById(R.id.editText_email_forgetPass);
+
+        btnResetPassword.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.btn_forget_password:
+
+                forgetPassViewModel.sendEmail(eTxtPassword.getText().toString());
+                break;
+
+            case R.id.btn_Cancel_forgetPass:
+
+                break;
+
+
+        }
+
+    }
+
+    /**
+     *  to get an Object from RegisterViewModel
+     */
+
+    class MyViewModelFactory implements ViewModelProvider.Factory {
+        private Activity mActivity;
+
+
+        public MyViewModelFactory(Activity activity) {
+            mActivity = activity;
+        }
+
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            return (T) new ForgetPassViewModel(mActivity);
+        }
     }
 }
