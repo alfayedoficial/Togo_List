4d3d846e97deb05a9c83163ebdb2a522245d6a2f
Bassem <Bsaleh103@gmail.com>
2019-10-13 16:22:46 +0200

    update

---
+++ app/src/main/java/com/group4/togolist/viewmodel/addFormViewModel.java
@@ -0,0 +1,23 @@
+package com.group4.togolist.viewmodel;
+
+import android.app.Activity;
+
+import androidx.lifecycle.ViewModel;
+
+public class addFormViewModel extends ViewModel {
+    /**
+     * class to handle add form view
+     */
+
+    private Activity activity;
+
+    public addFormViewModel(Activity activity){
+        this.activity = activity;
+    }
+
+    public void createNewTrip(){
+
+    }
+
+
+}
