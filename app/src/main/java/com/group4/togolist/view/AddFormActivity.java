d81908b5d3659c05f667875fa1384e26bbbcff2f
alialfayed1 <alialfayed1@gmail.com>
2019-10-13 20:13:49 +0200

    Add add bottom bar,

--- app/src/main/java/com/group4/togolist/view/AddFormActivity.java
+++ app/src/main/java/com/group4/togolist/view/AddFormActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
@@ -29,8 +30,7 @@ public class AddFormActivity extends AppCompatActivity {
 
         final PlaceAutocompleteFragment autocompleteFragment1 = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.editText_startPoint);
-        autocompleteFragment1.getView().setBackgroundColor(getResources().getColor(R.color.background_offwhite));
-//        autocompleteFragment1.setText("Start Point");
+        autocompleteFragment1.getView().setBackgroundColor(Color.WHITE);
         autocompleteFragment1.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
@@ -43,28 +43,36 @@ public class AddFormActivity extends AppCompatActivity {
             @Override
             public void onError(Status status) {
 
-                final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
-                        getFragmentManager().findFragmentById(R.id.editText_endPoint);
-                autocompleteFragment2.getView().setBackgroundColor(getResources().getColor(R.color.background_offwhite));
-//                autocompleteFragment2.setText("End Point");
-                autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-                    @Override
-                    public void onPlaceSelected(Place place) {
-                        // TODO: Get info about the selected place.
-                        latLang2 = place.getLatLng();
-                        placeDestination = (String) place.getName();
-                        long2 = latLang2.longitude;
-                        lat2 = latLang2.latitude;
-
-                    }
-
-                    @Override
-                    public void onError(Status status) {
-
-                    }
-                });
             }
         });
+        final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.editText_endPoint);
+        autocompleteFragment2.getView().setBackgroundColor(Color.WHITE);
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                latLang2 = place.getLatLng();
+                placeDestination = (String) place.getName();
+                long2 = latLang2.longitude;
+                lat2 = latLang2.latitude;
+
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+        });
+
+        AutocompleteFilter typeFilter1 = new AutocompleteFilter.Builder()
+                .setCountry("EG")
+                .build();
+        autocompleteFragment1.setFilter(typeFilter1);
+        AutocompleteFilter typeFilter2 = new AutocompleteFilter.Builder()
+                .setCountry("EG")
+                .build();
+        autocompleteFragment2.setFilter(typeFilter2);
     }
 
 }
