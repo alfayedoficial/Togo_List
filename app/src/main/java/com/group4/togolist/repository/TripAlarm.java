a4c1d52587261a107c384ff7044f3ebc322c82c7
Bassem <Bsaleh103@gmail.com>
2019-10-15 19:48:41 +0200

    update

--- app/src/main/java/com/group4/togolist/repository/TripAlarm.java
+++ app/src/main/java/com/group4/togolist/repository/TripAlarm.java
@@ -14,6 +14,7 @@ public class TripAlarm extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String tripName = intent.getExtras().getString(TRIP_NAME);
         Intent dialogIntent = new Intent(context, DialogActivity.class);
+        dialogIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         dialogIntent.putExtra(TRIP_NAME,tripName);
         context.startActivity(dialogIntent);
     }
