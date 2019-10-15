75e25715b5338f42e68a884b2ceb128cc3295dce
Laptop Shop <fatma_kamal@mans.edu.eg>
2019-10-15 19:38:27 +0200

    coding for DialogActivity

--- app/src/main/java/com/group4/togolist/view/DialogActivity.java
+++ app/src/main/java/com/group4/togolist/view/DialogActivity.java
@@ -1,16 +1,113 @@
 package com.group4.togolist.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.group4.togolist.R;
+import com.group4.togolist.viewmodel.DialogViewModel;
+import com.group4.togolist.viewmodel.LoginViewModel;
+import com.group4.togolist.viewmodel.ProfileViewModel;
 
-public class DialogActivity extends AppCompatActivity {
+public class DialogActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DialogViewModel dialogViewModel;
+    private Button btnStart , btnLater , btnCancel;
+    private TextView txtTripPlace , txtTripName , txtTripDate , txtTripTime;
+    private ImageButton imgBtnDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+
+        dialogViewModel = ViewModelProviders.of(this, new MyViewModelFactory(DialogActivity.this)).get(DialogViewModel.class);
+
+        initComponent();
+
+    }
+
+    /**
+     * initiate views
+     */
+    private void initComponent() {
+
+        txtTripName = findViewById(R.id.textViewPastTripName);
+        txtTripPlace = findViewById(R.id.textViewPastTripPlace);
+        txtTripDate = findViewById(R.id.textViewPastTripCalender);
+        txtTripTime = findViewById(R.id.textViewPastTripTime);
+
+        imgBtnDetails = findViewById(R.id.imageBtnDetails);
+        btnStart = findViewById(R.id.btnDialogStart);
+        btnLater = findViewById(R.id.btnDialogLater);
+        btnCancel = findViewById(R.id.btnDialogCancel);
+
+        imgBtnDetails.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+        btnLater.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDialogStart:
+                dialogViewModel.startTrip();
+                break;
+            case R.id.btnDialogLater:
+                dialogViewModel.waitForLater();
+                break;
+            case R.id.btnDialogCancel:
+                dialogViewModel.cancelTrip();
+                break;
+            case R.id.imageBtnDetails:
+                dialogViewModel.showDetails();
+                break;
+        }
+
+    }
+
+    /**
+     *
+     * @param tripName assign Trip Name to textView for TripName
+     * @param tripPlace assign Trip Place to textView for TripPlace
+     * @param tripDate assign Trip Date to textView for TripDate
+     * @param tripTime assign Trip Time to textView for TripTime
+     */
+    public void setDialogTripData(String tripName , String tripPlace , String tripDate , String tripTime){
+
+        txtTripName.setText(tripName);
+        txtTripDate.setText(tripPlace);
+        txtTripDate.setText(tripDate);
+        txtTripTime.setText(tripTime);
+
+    }
+
+    /**
+     *  DialogViewModelFactory
+     */
+
+    class MyViewModelFactory implements ViewModelProvider.Factory {
+        private Activity mActivity;
+
+
+        public MyViewModelFactory(Activity activity) {
+            mActivity = activity;
+        }
+
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            return (T) new DialogViewModel(mActivity);
+        }
+
     }
 }
