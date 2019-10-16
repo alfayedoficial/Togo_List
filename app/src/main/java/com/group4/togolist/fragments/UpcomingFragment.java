3f40f12530f0c3a272137d8f2a05ed0d635034d4
Laptop Shop <fatma_kamal@mans.edu.eg>
2019-10-16 07:46:00 +0200

    adding some changes

--- app/src/main/java/com/group4/togolist/fragments/UpcomingFragment.java
+++ app/src/main/java/com/group4/togolist/fragments/UpcomingFragment.java
@@ -16,21 +16,28 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.group4.togolist.R;
+import com.group4.togolist.model.HistoryAdapter;
 import com.group4.togolist.model.Trip;
 import com.group4.togolist.view.HomeRecyclerViewAdapter;
+import com.group4.togolist.viewmodel.HomeViewModel;
 
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UpcomingFragment extends Fragment {
+public class UpcomingFragment extends Fragment  implements HistoryAdapter.OnItemListener {
 
     private RecyclerView recyclerViewUpcomingTrip;
     private List<Trip> upcomingTrip ;
 
-    public UpcomingFragment() {
+    Context context;
+    HomeViewModel homeViewModel;
+
+    public UpcomingFragment(Context context, List<Trip> trips, HomeViewModel homeViewModel) {
         // Required empty public constructor
+        this.context = context;
+        upcomingTrip = trips;
     }
 
 
@@ -38,9 +45,12 @@ public class UpcomingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_upcoming,container,false);
+
         recyclerViewUpcomingTrip = view.findViewById(R.id.RecycleUpcoming);
 
-        HomeRecyclerViewAdapter adapter = new HomeRecyclerViewAdapter();
+        LinearLayoutManager lm = new LinearLayoutManager(context);
+        recyclerViewUpcomingTrip.setLayoutManager(lm);
+        HomeRecyclerViewAdapter adapter = new HomeRecyclerViewAdapter((HomeRecyclerViewAdapter.OnItemListener) this, context);
 
         adapter.setUpcomingTrip(upcomingTrip);
 
@@ -52,6 +62,11 @@ public class UpcomingFragment extends Fragment {
 
     public void setUpcomingTrip(List<Trip> upcomingTrip) {
         this.upcomingTrip = upcomingTrip;
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        homeViewModel.endedTripItemClicked(position);
     }
 
 }
