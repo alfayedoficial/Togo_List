b2a7423d223fc1e415ce4bd3949e3c2605087b21
Bassem <Bsaleh103@gmail.com>
2019-10-17 15:52:26 +0200

    dialog update

---
+++ app/src/main/java/com/group4/togolist/util/NotificationHelper.java
@@ -0,0 +1,50 @@
+package com.group4.togolist.util;
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import com.group4.togolist.R;
+import com.group4.togolist.model.Trip;
+
+public class NotificationHelper extends ContextWrapper {
+    public static final String channelID = "channelID";
+    public static final String channelName = "Channel Name";
+
+    private NotificationManager mManager;
+    private Trip currentTrip;
+    public NotificationHelper(Context base, Trip currentTrip) {
+        super(base);
+        this.currentTrip = currentTrip;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (mManager == null) {
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return mManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification() {
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(currentTrip.getTripName())
+                .setSmallIcon(R.drawable.logo);
+    }
+}
