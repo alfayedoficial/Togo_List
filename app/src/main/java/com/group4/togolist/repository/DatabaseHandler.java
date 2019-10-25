f5cb67daece70b3fea3c221df190bbe22b209858
Laptop Shop <fatma_kamal@mans.edu.eg>
2019-10-25 15:41:52 +0200

    add FancyToast

--- app/src/main/java/com/group4/togolist/repository/DatabaseHandler.java
+++ app/src/main/java/com/group4/togolist/repository/DatabaseHandler.java
@@ -2,13 +2,22 @@ package com.group4.togolist.repository;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.group4.togolist.db.TripDao;
 import com.group4.togolist.db.TripDatabase;
 import com.group4.togolist.model.Trip;
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -18,6 +27,9 @@ import java.util.concurrent.ExecutionException;
 
 public class DatabaseHandler {
     private TripDao daoInstance;
+    private DatabaseReference databaseReference ;
+
+
 
     /**
      * class Constructor
@@ -31,6 +43,11 @@ public class DatabaseHandler {
         if(daoInstance == null){
             TripDatabase databaseInstance = TripDatabase.getDataBaseInstance(activity);
             daoInstance = databaseInstance.getDaoInstance();
+
+            /**
+             * fireBase initiate the DB
+             */
+
         }
     }
 
@@ -49,6 +66,24 @@ public class DatabaseHandler {
 
     public void updateTrip(Trip trip){
         new UpdateTrip(trip).execute();
+    }
+
+    public List<Trip> getAllTrips(){
+        List<Trip> allTrips = new ArrayList<>();
+        class GetAllTrips extends AsyncTask<Void,Void,List<Trip>>{
+            @Override
+            protected List<Trip> doInBackground(Void... voids) {
+                return daoInstance.getTrips();
+            }
+        }
+        try {
+            allTrips = new GetAllTrips().execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return allTrips ;
     }
 
     public void getTripsByStatus(final MutableLiveData<List<Trip>> tripList, final String status)  {
@@ -71,6 +106,7 @@ public class DatabaseHandler {
     public void deleteTrip(Trip trip){
          new DeleteTrip(trip).execute();
     }
+
 
 
     /**
@@ -114,6 +150,12 @@ public class DatabaseHandler {
         @Override
         protected Void doInBackground(Void... voids) {
             daoInstance.updateTrip(trip);
+            /**
+             *
+             */
+//            firbaseHelp();
+//            databaseReference.child("Users").child(user.getUid()).child(trip.getId() + "").setValue(trip);
+//            Log.i("DatabaseHandler" ,trip.getId() + "" );
             return null;
         }
     }
@@ -123,19 +165,99 @@ public class DatabaseHandler {
      */
 
 
-    class DeleteTrip extends AsyncTask<Void,Void,Void>{
+    class DeleteTrip extends AsyncTask<Void,Void,Void> {
         Trip trip;
-        public DeleteTrip(Trip trip){
+
+        public DeleteTrip(Trip trip) {
             this.trip = trip;
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
             daoInstance.delete(trip);
+
+            /**
+             *
+                    firbaseHelp();
+                  databaseReference.child("Users").child(user.getUid()).child(trip.getId() + "").removeValue();
+             */
+
             return null;
         }
     }
 
+        /**
+         *  sync with firebase
+         */
 
+        class SyncWithFireBase extends AsyncTask<Void,Void,Void>{
+            List<Trip> tripList;
+            String userID ;
+            public SyncWithFireBase(List<Trip> tripList , String userID){
+                this.tripList = tripList;
+                this.userID = userID ;
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                /**
+                 *  getting all trips from Room ad upload it to FireBase Database
+                 */
+
+                fireBaseHelp();
+
+            if ( userID != null) {
+                databaseReference.child("Users").child(userID).removeValue();
+                for (Trip trip : tripList) {
+                    databaseReference.child("Users").child(userID).child(trip.getId() + "").setValue(trip);
+
+                }
+            }else
+                Log.i("User Id " , " user id is null ");
+
+                return null;
+            }
+    }
+
+    public void syncWithFireBase(String userId){
+            new SyncWithFireBase( getAllTrips(),userId).execute();
+    }
+
+    public void loadFromFireBase( String userID) {
+
+          fireBaseHelp();
+          if (userID != null) {
+              databaseReference.child("Users").child(userID).addValueEventListener(new ValueEventListener() {
+                  @Override
+                  public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                      if (!(dataSnapshot.getChildrenCount() == 0)) {
+                          Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                          for (DataSnapshot dSTrip : children) {
+                              Trip trip = dSTrip.getValue(Trip.class);
+                              deleteTrip(trip);
+                              addTrip(trip);
+                          }
+                      }
+                  }
+
+                  @Override
+                  public void onCancelled(@NonNull DatabaseError databaseError) {
+                      Log.i("DataBaseHandler", databaseError.toString());
+                  }
+              });
+          }
+
+    }
+
+    private void fireBaseHelp(){
+
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                databaseReference = firebaseDatabase.getReference();
+
+
+        }
 
 }
