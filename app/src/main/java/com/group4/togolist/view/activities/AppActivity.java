00fe3c6422a37aff618858a2c818e6e3a2ea9a31
Bassem <Bsaleh103@gmail.com>
2019-10-25 20:33:27 +0200

    app is working

--- app/src/main/java/com/group4/togolist/view/activities/AppActivity.java
+++ app/src/main/java/com/group4/togolist/view/activities/AppActivity.java
@@ -17,12 +17,13 @@ import com.group4.togolist.R;
 import com.group4.togolist.viewmodel.AppViewModel;
 import com.group4.togolist.viewmodel.HomeViewModel;
 
-public class AppActivity extends AppCompatActivity  implements View.OnClickListener {
+public class AppActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnPrivacyLayout , btnInfoLayout;
-//    private ImageButton imgBtnHome, imgBtnProfile ;
+    private Button btnPrivacyLayout, btnInfoLayout;
+    //    private ImageButton imgBtnHome, imgBtnProfile ;
 //    private FloatingActionButton  fltBtnAdd;
     private AppViewModel appViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +31,6 @@ public class AppActivity extends AppCompatActivity  implements View.OnClickListe
         getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         initComponent();
         appViewModel = ViewModelProviders.of(this, new MyViewModelFactory(AppActivity.this)).get(AppViewModel.class);
-
     }
 
     private void initComponent() {
@@ -45,8 +45,6 @@ public class AppActivity extends AppCompatActivity  implements View.OnClickListe
 //        fltBtnAdd.setOnClickListener(this);
         btnPrivacyLayout.setOnClickListener(this);
         btnInfoLayout.setOnClickListener(this);
-
-
     }
 
     @Override
@@ -64,7 +62,7 @@ public class AppActivity extends AppCompatActivity  implements View.OnClickListe
             case R.id.btnPrivacyLayout:
                 appViewModel.goToPrivacyLayout();
                 break;
-            case  R.id.btnInfoLayout:
+            case R.id.btnInfoLayout:
                 appViewModel.goToInfoLayout();
                 break;
         }
@@ -73,11 +71,9 @@ public class AppActivity extends AppCompatActivity  implements View.OnClickListe
     class MyViewModelFactory implements ViewModelProvider.Factory {
         private AppActivity mActivity;
 
-
         public MyViewModelFactory(AppActivity activity) {
             mActivity = activity;
         }
-
 
         @Override
         public <T extends ViewModel> T create(Class<T> modelClass) {
