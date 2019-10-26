37e27dfe37d92276e9dd98f891637d22a6828130
Laptop Shop <fatma_kamal@mans.edu.eg>
2019-10-26 22:46:16 +0200

    add map fragment code

--- app/src/main/java/com/group4/togolist/view/fragments/HistoryFragment.java
+++ app/src/main/java/com/group4/togolist/view/fragments/HistoryFragment.java
@@ -4,49 +4,83 @@ package com.group4.togolist.view.fragments;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.group4.togolist.R;
 import com.group4.togolist.view.adapters.HistoryAdapter;
 import com.group4.togolist.model.Trip;
 import com.group4.togolist.viewmodel.HomeViewModel;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HistoryFragment extends Fragment  implements HistoryAdapter.OnItemListener {
+public class HistoryFragment extends Fragment  implements HistoryAdapter.OnItemListener , OnMapReadyCallback {
 
    private List<Trip> pastTrips;
    private Context context;
    private HomeViewModel homeViewModel;
    private HistoryAdapter historyAdapter;
 
+   private GoogleMap myMap;
+
+   private SupportMapFragment  mMapFragment ;
+ private float markerBitMap [] = {BitmapDescriptorFactory.HUE_AZURE , BitmapDescriptorFactory.HUE_BLUE , BitmapDescriptorFactory.HUE_CYAN , BitmapDescriptorFactory.HUE_GREEN,
+         BitmapDescriptorFactory.HUE_MAGENTA , BitmapDescriptorFactory.HUE_ORANGE , BitmapDescriptorFactory.HUE_RED , BitmapDescriptorFactory.HUE_ROSE,
+         BitmapDescriptorFactory.HUE_VIOLET , BitmapDescriptorFactory.HUE_YELLOW};
+    public HistoryFragment(){
+           // Required empty public constructor
+       }
     public HistoryFragment(Context context, HomeViewModel homeViewModel) {
-        // Required empty public constructor
+
         this.context = context;
         this.homeViewModel = homeViewModel ;
     }
 
-
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,   ViewGroup container,
+                            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_history, container, false);
         RecyclerView pastTripRecyclerView = view.findViewById(R.id.RecyclePastTrip);
+
+        /**
+         *
+         */
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        /**
+         *
+         */
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
         pastTripRecyclerView.setLayoutManager(linearLayoutManager);
 
@@ -121,7 +155,66 @@ public class HistoryFragment extends Fragment  implements HistoryAdapter.OnItemL
             public void onChanged(List<Trip> trips) {
                 historyAdapter.setPastTrips(trips);
                 pastTrips = trips;
+                drawRoutes();
             }
         });
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap ;
+
+    }
+
+    private  int getColor(){
+        Random rnd = new Random();
+         int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        return color ;
+
+
+    }
+
+    private void drawRoutes(){
+        if (pastTrips !=null) {
+            if (pastTrips.size() != 0) {
+                int i = 0 ;
+                for (Trip trip : pastTrips) {
+
+                    myMap.addMarker(new MarkerOptions().position(new LatLng(trip.getStartLocationLatitude(), trip.getStartLocationLongitude())).title(trip.getTripName()).icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[i])));
+                    myMap.addMarker(new MarkerOptions().position(new LatLng(trip.getEndLocationLatitude(), trip.getEndLocationLongitude())).title(trip.getTripName()).icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[i])));
+                    myMap.addPolyline(new PolylineOptions()
+                            .add(new LatLng(trip.getStartLocationLatitude(), trip.getStartLocationLongitude()), new LatLng(trip.getEndLocationLatitude(), trip.getEndLocationLongitude()))
+                            .width(5)
+                            .color(getColor()));
+                    Log.i("MapRoute" , ""+ trip.getTripName());
+
+                    if ( i == markerBitMap.length -1)
+                        i = 0 ;
+                }
+            }
+        }
+
+        /**
+         * test code
+         *
+         *
+                int color = getColor();
+                myMap.addPolyline(new PolylineOptions()
+                        .add(new LatLng(28.4956014,34.4829166), new LatLng(25.0684143,34.8666785))
+                        .width(5)
+                        .color(color));
+                myMap.addMarker(new MarkerOptions().position(new LatLng(28.4956014,34.4829166)).title("Fatma").icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[0])));
+
+                myMap.addMarker(new MarkerOptions().position(new LatLng(25.0684143,34.8666785)).title("Fatma").icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[0])));
+                int colorDes = getColor();
+                myMap.addPolyline(new PolylineOptions()
+                        .add(new LatLng(31.0413814,31.3478201), new LatLng(31.3366173,27.1852923))
+                        .width(5)
+                        .color(colorDes));
+                myMap.addMarker(new MarkerOptions().position(new LatLng(31.0413814,31.3478201)).title("Kaml").icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[1])));
+                myMap.addMarker(new MarkerOptions().position(new LatLng(31.3366173,27.1852923)).title("Kamal").icon(BitmapDescriptorFactory.defaultMarker(markerBitMap[1])));
+            */
+
+    }
+
 }
